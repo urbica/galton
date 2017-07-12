8b2eac0644993061263e8c64add27d1910660da9
George Lifchits <george.lifchits@gmail.com>
2017-07-12 11:34:32 -0400

    ./dist/bundle => ./src/server.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-const galton = require('./dist/bundle');
+const galton = require('./src/server.js');
 const minimist = require('minimist');
 const packagejson = require('./package.json');
 
